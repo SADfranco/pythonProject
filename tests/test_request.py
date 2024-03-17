7d20085600920254c3c63f05a9e20b15d8901834
Andrei Shabaev <shabaevandrew@gmail.com>
2024-03-17 15:15:49 +0300

    init commit

---
+++ tests/test_request.py
@@ -0,0 +1,14 @@
+import requests
+from jsonschema import validators
+#from configuration import SERVICE_URL
+from src.schema.post import POST_SCHEMA
+
+
+
+def test_getting_post():
+    response = requests.get(url='https://my-json-server.typicode.com/typicode/demo/posts')
+    recieved_post = response.json()
+    #assert response.status_code == 200
+    #assert len(response.json()) == 3
+    for item in recieved_post:
+        validators(item, POST_SCHEMA)
