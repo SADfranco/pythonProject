10803d0c72ed0c19597e56db678ffe0f58996627
Andrei Shabaev <shabaevandrew@gmail.com>
2024-03-17 16:55:31 +0300

    Add new test code 500

--- tests/test_request.py
+++ tests/test_request.py
@@ -9,4 +9,5 @@ def test_getting_post():
     response = requests.get(url='https://my-json-server.typicode.com/typicode/demo/posts')
     recieved_post = response.json()
     assert response.status_code == 200
+    assert response.status_code == 500
     assert len(response.json()) == 3
